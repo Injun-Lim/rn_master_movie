f57011cc03fcf70692ea4d5ac82b51a1506cd654
Injun-Lim <jz6865@naver.com>
2022-03-23 23:55:10 +0900

    Create Detail.js

---
+++ screens/Detail.js
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components/native";
+
+const View = styled.View``;
+const Text = styled.Text``;
+
+const Detail = () => (
+  <View>
+    <Text>Detail</Text>
+  </View>
+);
+
+export default Detail;
